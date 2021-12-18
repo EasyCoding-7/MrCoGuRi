85a139c43a6b97b8e997490428add751250436d8
GoodayTH <taehyeongkim77@gmail.com>
2021-12-18 11:43:30 +0900

    init project

---
+++ currentpriceapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from currentpriceapp.views import hello_world
+
+app_name = "currentpriceapp"
+
+urlpatterns = [
+    path('hello_world/', hello_world, name='currentprice')
+]
